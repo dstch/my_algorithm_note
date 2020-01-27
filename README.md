0738cee15fdb02f3256c04780322e982c438bc2f
YvetteQian <qianya_1990@163.com>
2020-01-28 00:00:40 +0800

    add kmp method

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # my_algorithm_note
-Record algorithm learning
+Record algorithm learning  
 1、KMP：https://www.zhihu.com/question/21923021/answer/281346746
---
+++ kmp.py
@@ -0,0 +1,50 @@
+'''
+@Author: your name
+@Date: 2020-01-27 23:32:18
+@LastEditTime : 2020-01-27 23:53:53
+@LastEditors  : Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \my_algorithm_note\kmp.py
+'''
+
+
+class KMP(object):
+    def __init__(self, t, p):
+        '''
+        @description: t:主字符串，p:匹配字符串
+        @param {type} 
+        @return: 
+        '''
+        self.t = t
+        self.p = p
+
+    def cal_next(self):
+        next = [-1 for _ in range(len(self.p))]
+        j = -1
+        for i in range(len(self.p)):
+            if j == -1 or self.p[i] == self.p[j]:
+                i += 1
+                j += 1
+                next[i] = j
+            else:
+                j = next[j]
+        return next
+
+    def search(self):
+        i = j = 0
+        next = self.cal_next()
+        while i < len(self.t) and j < len(self.p):
+            if j == -1 or self.t[i] == self.p[j]:
+                i += 1
+                j += 1
+            else:
+                j = next[j]
+        if j == len(self.p):
+            return i - j
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    kmp = KMP('ababcabcacbab', 'abcac')
+    print(kmp.search())
